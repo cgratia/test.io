dac9b58d89c7bf9908c42e40871f845890d8c1c0
cgratia <clement.gratia@etu.u-bordeaux-montaigne.fr>
2024-05-31 17:18:56 +0200

    maj

--- js/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-    const slider = document.querySelector('.logo-slider');
-    const slide = document.querySelector('.logo-slide');
-
-    // Clone the slide content to create a seamless loop effect
-    const clone = slide.cloneNode(true);
-    slider.appendChild(clone);
-});
-
